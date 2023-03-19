31b582d2bc333ae1eba350e0f9db085c179936de
mahadiihasan <sagor.gsc15@gmail.com>
2023-03-20 00:17:31 +0600

    fundamentals of js must know

---
+++ fundamentals.js
@@ -0,0 +1,29 @@
+// ******** 6 fundamentals need to know ************
+
+// 1. How to declare variables using let and const
+// 2. if else condtion, logical operators (>, <, >=, <=, === , !==), AND (&&), OR (||)
+// 3. Array
+// 4. Loop
+// 5. Function
+// 6. Object
+
+
+
+
+
+const fatherName = 'Dulal Ahmed';
+let season = 'winter';
+season = 'summer';
+console.log(season);
+
+
+// 3 ways to access property by name
+
+const student = {
+
+    name : 'salman khan',
+    age : 60,
+    movies : ['tere naam', 'wanted']
+
+
+}
